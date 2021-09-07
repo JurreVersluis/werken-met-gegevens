374c776dd8de21a228edb276eeb20c2da9d15b80
Idioot <99066617@mydavinci.nl>
2021-09-07 13:51:40 +0200

    sommen

---
+++ berekening2.py
@@ -0,0 +1,5 @@
+Dekosten0= 4 * 7.45
+tijd= 45 / 5
+dekosten2= (tijd * 0.37) * 4
+dekosten = (Dekosten0) + (dekosten2)
+print (dekosten)
---
+++ main.py
@@ -0,0 +1,8 @@
+croissantjes = 17
+kosten1 = 0.39
+stokbroden = 2
+kosten2 = 2.78
+korting = 1.50
+
+dekosten = (croissantjes * kosten1) + (stokbroden * kosten2) - korting
+print(dekosten)
