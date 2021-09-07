12653b432f406d0526c033ca0bbc302d586d0cd1
Idioot <99066617@mydavinci.nl>
2021-09-07 14:09:11 +0200

    verbeterdeberekeningen

---
+++ berekening1verbeterd.py
@@ -0,0 +1,8 @@
+croissantjes = 17
+kosten1 = 0.39
+stokbroden = 2
+kosten2 = 2.78
+korting = 1.50
+
+dekosten = (croissantjes * kosten1) + (stokbroden * kosten2) - korting
+print("De feestlunch kost je bij de bakker " + str(dekosten) + " euro voor de 17 croissantjes en de 2 stokbroden als de 3 kortingsbonnen nog geldig zijn!")
---
+++ berekening2verbeterd2.py
@@ -0,0 +1,6 @@
+Dekosten0= 4 * 7.45
+tijd= 45 / 5
+dekosten2= (tijd * 0.37) * 4
+dekosten = (Dekosten0) + (dekosten2)
+dekosten= round(dekosten,2)
+print ("Dit geweldige dagje-uit met 4 mensen in de Speelhal met 45 minuten VR kost je maar " + (str(dekosten) + " euro"))
